99d995184decfbad7393ae9e93211a27bdaf0f00
brunosj <contact@landozone.net>
2023-04-24 23:06:46 +0200

    getAllShows

--- src/common/context/global-context-provider.js
+++ src/common/context/global-context-provider.js
@@ -1,59 +1,57 @@
-import React from "react"
+import React from 'react';
 
-export const GlobalStateContext = React.createContext(undefined)
-export const GlobalDispatchContext = React.createContext(undefined)
+export const GlobalStateContext = React.createContext(undefined);
+export const GlobalDispatchContext = React.createContext(undefined);
 
 const initialState = {
-  theme: "light",
+  theme: 'light',
   isLiveRadioPlaying: false,
   archivePlays: null,
-  isArchivPlaying: true
-}
+  isArchivPlaying: true,
+};
 
 function reducer(state, action) {
-  // console.log(state, action)
   switch (action.type) {
-    case "TOGGLE_THEME": {
+    case 'TOGGLE_THEME': {
       return {
         ...state,
-        theme: state.theme === "light" ? "dark" : "light",
-      }
+        theme: state.theme === 'light' ? 'dark' : 'light',
+      };
     }
-    case "PAUSE_LIVE_RADIO": {
+    case 'PAUSE_LIVE_RADIO': {
       return {
         ...state,
-        isLiveRadioPlaying: false
-      }
+        isLiveRadioPlaying: false,
+      };
     }
-    case "PlAY_LIVE_RADIO": {
+    case 'PlAY_LIVE_RADIO': {
       return {
         ...state,
         isLiveRadioPlaying: true,
-        archivePlays: null
-      }
+        archivePlays: null,
+      };
     }
-    case "TOGGLE_ARCHIVE_PLAYER": {
-      const archivePlays = action.payload === state.archivePlays
-        ? null
-        : action.payload
+    case 'TOGGLE_ARCHIVE_PLAYER': {
+      const archivePlays =
+        action.payload === state.archivePlays ? null : action.payload;
 
       const isLiveRadioPlaying = !state.archivePlays
         ? false
-        : state.isLiveRadioPlaying
+        : state.isLiveRadioPlaying;
 
       return {
         ...state,
         archivePlays,
-        isLiveRadioPlaying
-      }
+        isLiveRadioPlaying,
+      };
     }
     default:
-      throw new Error("Bad Action Type")
+      throw new Error('Bad Action Type');
   }
 }
 
 const GlobalContextProvider = ({ children }) => {
-  const [state, dispatch] = React.useReducer(reducer, initialState)
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
   return (
     <GlobalStateContext.Provider value={state}>
@@ -61,7 +59,7 @@ const GlobalContextProvider = ({ children }) => {
         {children}
       </GlobalDispatchContext.Provider>
     </GlobalStateContext.Provider>
-  )
-}
+  );
+};
 
-export default GlobalContextProvider
+export default GlobalContextProvider;
