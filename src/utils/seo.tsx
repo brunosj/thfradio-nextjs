c2b08630a7c81f12e93512f33188acd9181b0d52
brunosj <contact@landozone.net>
2023-06-19 11:54:31 +0200

    seo image

--- src/utils/seo.tsx
+++ src/utils/seo.tsx
@@ -16,9 +16,11 @@ export const SEOComponent = ({
       <meta property='og:title' content={siteMetadata.title} />
       <meta property='og:url' content={siteMetadata.siteUrl} />
       <meta property='og:description' content={siteMetadata.description} />
+      <meta property='og:image' content={siteMetadata.image} />
       <meta property='og:type' content='website' />
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:title' content={siteMetadata.title} />
+      <meta name='twitter:image' content={siteMetadata.image} />
       <meta name='twitter:description' content={siteMetadata.description} />
     </Head>
   );
