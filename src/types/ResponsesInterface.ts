a62844b7fb84a3c8337eb18757ace28d86073a9f
brunosj <contact@landozone.net>
2023-06-22 11:58:00 +0200

    button color types

--- src/types/ResponsesInterface.ts
+++ src/types/ResponsesInterface.ts
@@ -105,7 +105,7 @@ export interface AboutSection {
     id: number;
     text: string;
     path: string;
-    color: 'white' | 'blue';
+    color: 'white-orange' | 'white-blue' | 'blue';
   }>;
   links: {
     data: Array<{
