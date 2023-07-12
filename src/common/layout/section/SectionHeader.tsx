4211179c0f8733fa28f44491e7bbca24339b5ab0
brunosj <contact@landozone.net>
2023-07-12 17:35:28 +0200

    fixes 12.07

--- src/common/layout/section/SectionHeader.tsx
+++ src/common/layout/section/SectionHeader.tsx
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown';
+import { ReactNode } from 'react';
 
 type SectionHeaderProps = {
   title: string;
@@ -6,11 +7,17 @@ type SectionHeaderProps = {
 };
 
 const SectionHeader = ({ title, text }: SectionHeaderProps) => {
+  const components = {
+    a: ({ ...props }) => (
+      <a {...props} target='_blank' rel='noopener noreferrer' />
+    ),
+  };
+
   return (
     <div className='layout text-white text-center sectionPy max-w-4xl m-auto'>
       <h1 className='uppercase font-normal'>{title}</h1>
       <h4 className='font-mono font-light markdown'>
-        <ReactMarkdown>{text}</ReactMarkdown>
+        <ReactMarkdown components={components}>{text}</ReactMarkdown>
       </h4>
     </div>
   );
--- src/modules/news/HomeNewsSection.tsx
+++ src/modules/news/HomeNewsSection.tsx
@@ -5,15 +5,24 @@ import { useTranslation } from 'next-i18next';
 import NewsCarousel from '../carousel/NewsCarousel';
 import { DataContext } from '@/context/DataContext';
 import { useContext } from 'react';
+import BarsSpinner from '@/common/ui/BarsSpinner';
+import { useState, useEffect } from 'react';
 
-interface ShowsProps {
+interface NewsProps {
   title: string;
   text: string;
 }
 
-const HomeNewsSection = ({ title, text }: ShowsProps) => {
-  const { t } = useTranslation();
+const HomeNewsSection = ({ title, text }: NewsProps) => {
   const { news } = useContext(DataContext)!;
+  const [isLoading, setIsLoading] = useState(true);
+  const { t } = useTranslation();
+  useEffect(() => {
+    if (news && news.length > 0) {
+      setIsLoading(false);
+    }
+  }, [news]);
+
   const newsArray = news.sort(
     (a: NewsType, b: NewsType) =>
       new Date(b.attributes.date).getTime() -
@@ -24,7 +33,13 @@ const HomeNewsSection = ({ title, text }: ShowsProps) => {
     <section className='bg-darkBlue sectionPb'>
       <SectionHeader title={title} text={text} />
       <div className='layout'>
-        <NewsCarousel slides={newsArray} />
+        {isLoading ? (
+          <div className='flex justify-center pb-12'>
+            <BarsSpinner color='#1200ff' />
+          </div>
+        ) : (
+          <NewsCarousel slides={newsArray} />
+        )}
       </div>
       <div className='pt-12 flex justify-center'>
         <Button
