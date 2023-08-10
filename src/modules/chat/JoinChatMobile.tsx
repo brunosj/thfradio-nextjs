ae4e342a832655b67cd6eb42e74a4ff1c962b2ff
brunosj <contact@landozone.net>
2023-08-10 16:38:01 +0200

    chat mobile menu fix

--- src/modules/chat/JoinChatMobile.tsx
+++ src/modules/chat/JoinChatMobile.tsx
@@ -7,7 +7,7 @@ import { TbBrandDiscord } from 'react-icons/tb';
 
 export default function JoinChatMobile() {
   return (
-    <div className='fixed bottom-40  z-50'>
+    <div className='pb-5 pt-2'>
       <button className='inline-flex items-center p-3 rounded-xl bg-orange-500 text-white focus:outline-none focus:ring-4 font-mono font-semibold border border-white'>
         <Link href='/chat' target='_blank'>
           Join the chat!
