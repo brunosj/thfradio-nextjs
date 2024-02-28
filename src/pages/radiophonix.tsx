989e9cf18a1351f690f8a306887aba6d68055125
brunosj <contact@landozone.net>
2024-02-27 21:23:23 -0500

    radiophonix updates

--- src/pages/radiophonix.tsx
+++ src/pages/radiophonix.tsx
@@ -26,22 +26,22 @@ const Radiophonix: NextPage<{
           <div className='layout sectionPy'>
             <h1 className='text-white'>{page.attributes.title}</h1>
           </div>
-          <div className='bg-darkBlue layout grid grid-cols-1 lg:grid-cols-2 sectionPy gap-6 lg:gap-12'>
+          <div className='bg-darkBlue layout grid grid-cols-1 lg:grid-cols-2 sectionPy gap-6 lg:gap-24'>
             <div className='markdown lg:max-w-5xl m-auto text-white'>
               <ReactMarkdown>{page.attributes.description}</ReactMarkdown>
             </div>
-            <div className='space-y-6 lg:space-y-12'>
-              <div className='relative w-full h-48 lg:h-72'>
+            <div className='space-y-6 lg:space-y-12 relative'>
+              <div className='relative w-full h-[25vh] lg:h-[50vh] rounded-xl'>
                 <Image
                   quality={50}
-                  src={`${CMS_URL}/uploads/radiophonix_1b39d51712.jpg`}
+                  src={`${CMS_URL}/uploads/20240208_PAULINE_IVO_blanc_f4c559f8d0.jpg`}
                   fill
                   sizes=''
                   className='object-cover object-center rounded-xl'
                   alt='THF Radio x Radiophonix'
                 />
               </div>
-              <div className='w-full lg:w-1/2 mx-auto'>
+              <div className='max-w-full lg:max-w-[60%] mx-auto'>
                 <div className='bg-white rounded-xl py-3 px-6 flex items-center gap-6 justify-between'>
                   <h4>Listen to stream here</h4>
                   <AudioPlayer
@@ -51,6 +51,20 @@ const Radiophonix: NextPage<{
                   />
                 </div>
               </div>
+              <a
+                className='relative w-full h-96 rounded-xl bg-white inline-flex'
+                href='https://www.phonix16.de/'
+                target='_blank'
+              >
+                <Image
+                  quality={50}
+                  src={`${CMS_URL}/uploads/PHONIX_16_logo_blue_RGB_PHOENIX_logo_blue_white_BG_fd424134b8.png`}
+                  fill
+                  sizes=''
+                  className='object-contain object-center rounded-xl p-12'
+                  alt='THF Radio x Radiophonix'
+                />
+              </a>
             </div>
           </div>
         </div>
